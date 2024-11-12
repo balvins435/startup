907ae7b8d1a86323f7451e7baeda840e313baa54
balvins435 <vinnbalvins@gmail.com>
2024-11-13 01:37:16 +0300

    initial commit

--- startup/settings.py
+++ startup/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1=+$^2p=!pjqzv)eia@9mqejh2s=q&bjlc#bl)#bhq6bngn&gc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
